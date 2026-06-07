1b39fb98da8ac5ea91c6cbc447fe71d9fe998b62
Nhlanhla Makhubu <52780351+Conroy-a11y@users.noreply.github.com>
2026-06-07 19:04:11 +0200

    Revert "Update splash_screen.dart"

--- splash_screen_assignment/lib/splash_screen.dart
+++ splash_screen_assignment/lib/splash_screen.dart
@@ -39,7 +39,7 @@ class _SplashScreenState extends State<SplashScreen> {
             Icon(Icons.restaurant, size: 120, color: Colors.deepOrange),
             const SizedBox(height: 20),
             const Text(
-              'Welcome To Food Lovers App Of KodeCamp',
+              'Welcome To Food Lovers App',
               style: TextStyle(
                 fontSize: 30,
                 fontWeight: FontWeight.bold,
