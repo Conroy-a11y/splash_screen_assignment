6438fa182f54064fb2422284f0f4390ab6e8fbb6
Conroy Makhubu <cmakhubu@gmail.com>
2026-06-07 16:07:19 +0200

    Create splash_screen.dart

---
+++ splash_screen_assignment/lib/splash_screen.dart
@@ -0,0 +1,54 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'profile_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  bool isLoading = true;
+
+  @override
+  void initState() {
+    super.initState();
+
+    Timer(const Duration(seconds: 3), () {
+      setState(() {
+        isLoading = false;
+      });
+
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const ProfileScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.orange.shade100,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.restaurant, size: 120, color: Colors.deepOrange),
+            const SizedBox(height: 20),
+            const Text(
+              'Food Lovers App',
+              style: TextStyle(
+                fontSize: 30,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepOrange,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
